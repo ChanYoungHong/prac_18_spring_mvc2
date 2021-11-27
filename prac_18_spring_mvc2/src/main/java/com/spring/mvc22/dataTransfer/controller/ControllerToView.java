031ba9577e202daf1c06a9a54b3b52649a2a081e
hongchan-yeong <hongchan-yeong@192.168.0.21>
2021-11-27 14:02:46 +0900

    1127 토 연

--- prac_18_spring_mvc2/src/main/java/com/spring/mvc22/dataTransfer/controller/ControllerToView.java
+++ prac_18_spring_mvc2/src/main/java/com/spring/mvc22/dataTransfer/controller/ControllerToView.java
@@ -5,10 +5,15 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.spring.mvc22.dataTransfer.domain.MemberDto;
@@ -67,7 +72,7 @@ public class ControllerToView {
 		return mv;
 	}
 	
-	
+
 	
 	// 3) httpServeletRequest
 	
@@ -92,4 +97,50 @@ public class ControllerToView {
 		
 		return "dataTransfer/memberList";
 	}
+	
+	// 4) ResponseBody
+	
+	@RequestMapping(value="/responseBodyEx", method=RequestMethod.GET)
+	public @ResponseBody String responseBody() {
+		
+		String message = "<h1> helloo nice to meet youe </h1>";
+		
+		return message;
+	}
+	
+	// 5) ResponseEntity 
+	@RequestMapping(value="responseEntityEx", method=RequestMethod.GET)
+	public ResponseEntity<Object>	responseEntityEx(){
+		
+		HttpHeaders responseHeader = new HttpHeaders();
+		responseHeader.add("ContentType", "text/html; charset=UTF-8");
+		
+		String data = "<h1>html 페이지반환 nice to meet you kkkkkk</h1>";
+		
+		return new ResponseEntity<Object>(data, responseHeader, HttpStatus.OK);
+	}
+	
+	
 }
+
+
+// 6) RestController 
+
+@RestController
+class RestContollerEx {
+	
+	@RequestMapping(value="/restControllerEx", method = RequestMethod.GET)
+	public String restControllerEx() {
+		
+		String  message = "<script>";
+		message += "alert('login fail!!!');";
+		message += "location.href='cTov/modelEx'";
+		message += "</script>";
+		
+		
+		return message;
+	}
+	
+}
+
+
